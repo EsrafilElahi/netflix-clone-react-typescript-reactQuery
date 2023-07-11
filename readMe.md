f477a731d1f0edf0face5ebfa7c65bce0844a1fb
esrafilelahi <essinho1121@gmail.com>
2023-07-11 20:19:58 +0330

    add other pages

---
+++ readMe.md
@@ -0,0 +1,5 @@
+netflix clone : 
+reactjs-vite
+typescript
+tailwind css
+react-query
---
+++ src/pages/Series.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Series = () => {
+  return (
+    <div>Series</div>
+  )
+}
+
+export default Series
