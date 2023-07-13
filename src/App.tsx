579b9eb32ba11dd4ecbf8f750086b9a16ec9880f
esrafilelahi <essinho1121@gmail.com>
2023-07-13 23:53:54 +0330

    add 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 // eslint-disable-next-line prettier/prettier
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
+import Series from './pages/Series';
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Home title='Home' />,
-	},
-	{
-		path: '/movies',
-		element: <Movies title='Movies' />,
-	},
+  {
+    path: '/',
+    element: <Home title='Home' />,
+  },
+  {
+    path: '/movies',
+    element: <Movies title='Movies' />,
+  },
+  {
+    path: '/series',
+    element: <Series title='Series' />,
+  },
 ]);
 
 interface AppProps {
-	title: string;
+  title: string;
 }
 
 const App: React.FC<AppProps> = () => {
-	return (
-		<>
-			<RouterProvider router={router} />
-		</>
-	);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,54 @@ import App from './App.tsx';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<ErrorBoundary>
-			<App title='Home' />
-		</ErrorBoundary>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App title='Home' />
+    </ErrorBoundary>
+  </React.StrictMode>
 );
+
+// const queryClient = new QueryClient();
+
+// // Define the "addTodo" mutation
+// queryClient.setMutationDefaults(['addTodo'], {
+// 	mutationFn: addTodo,
+// 	onMutate: async (variables) => {
+// 		// Cancel current queries for the todos list
+// 		await queryClient.cancelQueries({ queryKey: ['todos'] });
+
+// 		// Create optimistic todo
+// 		const optimisticTodo = { id: uuid(), title: variables.title };
+
+// 		// Add optimistic todo to todos list
+// 		queryClient.setQueryData(['todos'], (old) => [...old, optimisticTodo]);
+
+// 		// Return context with the optimistic todo
+// 		return { optimisticTodo };
+// 	},
+// 	onSuccess: (result, variables, context) => {
+// 		// Replace optimistic todo in the todos list with the result
+// 		queryClient.setQueryData(['todos'], (old) =>
+// 			old.map((todo) => (todo.id === context.optimisticTodo.id ? result : todo)),
+// 		);
+// 	},
+// 	onError: (error, variables, context) => {
+// 		// Remove optimistic todo from the todos list
+// 		queryClient.setQueryData(['todos'], (old) => old.filter((todo) => todo.id !== context.optimisticTodo.id));
+// 	},
+// 	retry: 3,
+// });
+
+// // Start mutation in some component:
+// const mutation = useMutation({ mutationKey: ['addTodo'] });
+// mutation.mutate({ title: 'title' });
+
+// // If the mutation has been paused because the device is for example offline,
+// // Then the paused mutation can be dehydrated when the application quits:
+// const state = dehydrate(queryClient);
+
+// // The mutation can then be hydrated again when the application is started:
+// hydrate(queryClient, state);
+
+// // Resume the paused mutations:
+// queryClient.resumePausedMutations();
