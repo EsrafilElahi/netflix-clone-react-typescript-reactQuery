2bac04690d9ad634e4d475b18f7a2190f79bd4ce
esrafilelahi <essinho1121@gmail.com>
2023-07-15 23:03:24 +0330

    add styles in path

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       'utils': path.resolve(__dirname, './src/utils'),
       'assets': path.resolve(__dirname, './src/assets'),
       'types': path.resolve(__dirname, './src/types'),
+      'styles': path.resolve(__dirname, './src/styles'),
     },
   },
 })
