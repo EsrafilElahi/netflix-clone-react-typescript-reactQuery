faf95b3bc5503107793c367b416129b74d791adb
esrafilelahi <essinho1121@gmail.com>
2023-07-16 21:57:30 +0330

    mvoieItem

--- src/components/Movie/MovieItem.tsx
+++ src/components/Movie/MovieItem.tsx
@@ -1,16 +1,44 @@
 import React,{ useState } from 'react';
 import styles from 'styles/pages_styles/Home.module.css';
-import { Result } from 'types/HomePageTypes';
+import { Result1,Result2 } from 'types/HomePageTypes';
 
-type RequiredValues = 'original_title' | 'poster_path' | 'release_date' | 'original_name' | 'first_air_date';
-type MovieItemProps = Partial<Result> & Required<Pick<Result, RequiredValues>>;
+// type RequiredValues = 'original_title' | 'poster_path' | 'release_date' | 'original_name' | 'first_air_date';
+// type MovieItemProps = Partial<Result> & Required<Pick<Result, RequiredValues>>;
 
-const MovieItem: React.FC<MovieItemProps> = (props) => {
-	const { original_title, original_name, release_date, first_air_date, poster_path } = props;
-  const [hovered, setHovered] = useState<boolean>(false);
-	const date: Date = new Date(release_date ?? first_air_date);
-	const year: number = date.getFullYear();
-	console.log(props);
+type MovieItemProp = {
+	item: Partial<Result1> | Partial<Result2>;
+};
+
+export const isResult1 = (value: Partial<Result1> | Partial<Result2>): value is Partial<Result1> => {
+	return 'original_name' in value && 'first_air_date' in value;
+};
+
+export const isResult2 = (value: Partial<Result1> | Partial<Result2>): value is Partial<Result2> => {
+	return 'original_title' in value && 'release_date' in value;
+};
+
+const MovieItem: React.FC<MovieItemProp> = (props) => {
+
+	// const { original_title, original_name, release_date, first_air_date, poster_path } = props.item;
+	const [hovered, setHovered] = useState<boolean>(false);
+	
+	if (isResult1(props.item)) {
+		console.log('props.item 1 :', props.item);
+	} else {
+		console.log('props.item 2 :', props.item);
+	}
+
+	const getYear = () => {
+		const releaseDate = isResult1(props.item) ? props.item.first_air_date : props.item.release_date;
+
+		if (releaseDate) {
+			const date: Date = new Date(releaseDate);
+			const year: number = date.getFullYear();
+			return year;
+		}
+
+		return null;
+	}; 
 
 	return (
 		<div
@@ -19,13 +47,15 @@ const MovieItem: React.FC<MovieItemProps> = (props) => {
 			onMouseLeave={() => setHovered(false)}
 		>
 			<div className={`absolute hidden ${styles.movieTitle} ${hovered ? styles.fadeIn : null}`}>
-				<span className='leading-[normal]'>{original_title ?? original_name}</span>
-				<span>{year}</span>
+				<span className='leading-[normal]'>
+					{isResult1(props.item) ? props.item.original_name : props.item.original_title}
+				</span>
+				<span>{getYear()}</span>
 			</div>
 			<div className='w-full h-full rounded-lg relative'>
 				<img
-					src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-					alt={original_title}
+					src={`https://image.tmdb.org/t/p/w500${props.item.poster_path}`}
+					alt={isResult1(props.item) ? props.item.original_name : props.item.original_title}
 					className='w-full h-full rounded-lg'
 				/>
 			</div>
