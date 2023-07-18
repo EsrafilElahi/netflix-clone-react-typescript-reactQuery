b3cced1f60df380a0ca2843ba0cb48b0ba84b4a4
esrafilelahi <essinho1121@gmail.com>
2023-07-18 23:08:02 +0330

    add url api

--- src/pages/Series.tsx
+++ src/pages/Series.tsx
@@ -17,7 +17,7 @@ const Series: React.FC<SeriesProps> = (props) => {
 
 	const fetchSeries = async (page: number) => {
 		const res = await fetchData(
-			`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc`
+			`https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&sort_by=popularity.desc`
 		);
 		return res;
 	};
