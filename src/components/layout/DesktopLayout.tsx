8c21e4272e57efb3efcf5fe659ba175e092feb91
esrafilelahi <essinho1121@gmail.com>
2023-07-18 23:00:00 +0330

    desktop

--- src/components/layout/DesktopLayout.tsx
+++ src/components/layout/DesktopLayout.tsx
@@ -1,16 +1,20 @@
 import Footer from 'components/header_footer/Footer';
 import Navbar from 'components/header_footer/Navbar';
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 interface DesktopLayoutProps {
 	children: React.ReactNode
 }
 
 const DesktopLayout: React.FC<DesktopLayoutProps> = ({ children }) => {
+	const params = useLocation();
+	console.log('params :', params);
+
 	return (
 		<div className='w-full h-full flex flex-col'>
 			<header className=''>
-				<Navbar />
+				<Navbar scrollable={params.pathname === '/'} />
 			</header>
 			<main className='grow'>{children}</main>
 			<footer className=''>
