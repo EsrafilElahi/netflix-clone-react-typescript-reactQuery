1e473ff2dc1d5540a8837651dba40d90e18d261d
esrafilelahi <essinho1121@gmail.com>
2023-07-19 15:47:03 +0330

    add select compo

--- src/components/other/Select.tsx
+++ src/components/other/Select.tsx
@@ -1,23 +1,19 @@
-import React,{ useState } from 'react';
-
-interface Fruit {
-	id: number;
-	name: string;
-}
+import React,{ useEffect,useState } from 'react';
+import { useQuery } from 'react-query';
+import { fetchData } from '../../api gateway/HomePage';
 
 
 interface SelectOption<T extends string> {
 	value: T;
 	label: string;
 }
-
 interface SelectProps<T extends string> {
 	options: SelectOption<T>[];
 	onChange: (selectedOption: SelectOption<T>) => void;
 }
 
 function Select<T extends string>(props: SelectProps<T>) {
-  const { options, onChange } = props;
+	const { options, onChange } = props;
 	const [selectedOption, setSelectedOption] = useState<SelectOption<T> | null>(null);
 
 	const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -29,6 +25,19 @@ function Select<T extends string>(props: SelectProps<T>) {
 			onChange(selectedOption);
 		}
 	};
+
+	const fetchGenre = async (genreID: number) => {
+		const res = await fetchData(`https://api.themoviedb.org/3/discover/movie?api_key={apiKey}&with_genres=${genreID}`);
+		return res;
+	};
+
+		const { isLoading, isError, error, data, isFetching } = useQuery<any>(['genre', '123'], () => fetchGenre(123));
+
+		console.log('dataaaaaaaa genre :', data);
+
+	useEffect(() => {
+		fetchGenre(123);
+	}, [selectedOption]);
 
 	return (
 		<select
--- src/utils/axios.ts
+++
@@ -1,7 +0,0 @@
-// axios.get<User[]>('http://localhost:8080/admin/users')
-//   .then(response => {
-//     console.log(response.data);
-//     // @ts-ignore
-//     setUserList(response.data);
-//   }
-// );
