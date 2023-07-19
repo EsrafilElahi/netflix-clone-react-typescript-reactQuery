756033968cf5a60f235a06dcdd9947bb3826964f
esrafilelahi <essinho1121@gmail.com>
2023-07-19 21:55:49 +0330

    fix bugs

--- src/components/todos.tsx
+++ src/components/todos.tsx
@@ -1,29 +1,29 @@
-const queryClient = useQueryClient();
+// const queryClient = useQueryClient();
 
-useMutation({
-	mutationFn: updateTodo,
-	// When mutate is called:
-	onMutate: async (newTodo) => {
-		// Cancel any outgoing refetches
-		// (so they don't overwrite our optimistic update)
-		await queryClient.cancelQueries({ queryKey: ['todos'] });
+// useMutation({
+// 	mutationFn: updateTodo,
+// 	// When mutate is called:
+// 	onMutate: async (newTodo) => {
+// 		// Cancel any outgoing refetches
+// 		// (so they don't overwrite our optimistic update)
+// 		await queryClient.cancelQueries({ queryKey: ['todos'] });
 
-		// Snapshot the previous value
-		const previousTodos = queryClient.getQueryData(['todos']);
+// 		// Snapshot the previous value
+// 		const previousTodos = queryClient.getQueryData(['todos']);
 
-		// Optimistically update to the new value
-		queryClient.setQueryData(['todos'], (old) => [...old, newTodo]);
+// 		// Optimistically update to the new value
+// 		queryClient.setQueryData(['todos'], (old) => [...old, newTodo]);
 
-		// Return a context object with the snapshotted value
-		return { previousTodos };
-	},
-	// If the mutation fails,
-	// use the context returned from onMutate to roll back
-	onError: (err, newTodo, context) => {
-		queryClient.setQueryData(['todos'], context.previousTodos);
-	},
-	// Always refetch after error or success:
-	onSettled: () => {
-		queryClient.invalidateQueries({ queryKey: ['todos'] });
-	},
-});
+// 		// Return a context object with the snapshotted value
+// 		return { previousTodos };
+// 	},
+// 	// If the mutation fails,
+// 	// use the context returned from onMutate to roll back
+// 	onError: (err, newTodo, context) => {
+// 		queryClient.setQueryData(['todos'], context.previousTodos);
+// 	},
+// 	// Always refetch after error or success:
+// 	onSettled: () => {
+// 		queryClient.invalidateQueries({ queryKey: ['todos'] });
+// 	},
+// });
